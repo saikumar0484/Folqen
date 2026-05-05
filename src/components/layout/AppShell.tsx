7b49713c2aa86c96e321e228c711c3175de0f527
RAYALA SAIKUMAR <118263890+saikumar0484@users.noreply.github.com>
2026-05-05 12:04:06 +0530

    fix: use default import for MiniAgentChat in AppShell

--- src/components/layout/AppShell.tsx
+++ src/components/layout/AppShell.tsx
@@ -1,7 +1,6 @@
 import { Sidebar } from './Sidebar'
 import { Topbar } from './Topbar'
-import { MiniAgentChat } from '@/components/MiniAgentChat'
-
+import MiniAgentChat from '@/components/MiniAgentChat'
 export function AppShell({ children }: { children: React.ReactNode }) {
   return (
     <div className="min-h-screen bg-[#080612] flex">
