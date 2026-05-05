5d59b6e1145c39432c9873824a254dc4d55b88bc
RAYALA SAIKUMAR <118263890+saikumar0484@users.noreply.github.com>
2026-05-05 10:58:15 +0530

    Phase 2: Add AppShell component

---
+++ src/components/layout/AppShell.tsx
@@ -0,0 +1,18 @@
+import { Sidebar } from './Sidebar'
+import { Topbar } from './Topbar'
+import { MiniAgentChat } from '@/components/agent/MiniAgentChat'
+
+export function AppShell({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="min-h-screen bg-[#080612] flex">
+      <Sidebar />
+      <div className="flex-1 ml-60 flex flex-col min-h-screen">
+        <Topbar />
+        <main className="flex-1 p-6 overflow-auto">
+          {children}
+        </main>
+      </div>
+      <MiniAgentChat />
+    </div>
+  )
+}
